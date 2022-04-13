0e747f2353c34ab2b6a84051ace7a3034f1d9f26
SebastianMayorgaCalderon <sebasmayorga7@gmail.com>
2022-04-12 20:30:01 -0600

    Added lazy loading

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-
 import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
 
 // Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
@@ -11,16 +10,19 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     createMemoryHistory({
       initialEntries: [initialPath],
     });
-  if (onNavigate) history.listen(onNavigate);
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
 
   ReactDOM.render(<App history={history} />, el);
 
   return {
-    onParentNavigate({ pathname: nextPathName }) {
+    onParentNavigate({ pathname: nextPathname }) {
       const { pathname } = history.location;
-      if (pathname !== nextPathName) {
-        console.log("Auth navigated");
-        history.push(nextPathName);
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
       }
     },
   };
