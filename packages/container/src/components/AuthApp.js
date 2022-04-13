213b07094b720969c112b1a0cb3dbb97c1c95847
SebastianMayorgaCalderon <sebasmayorga7@gmail.com>
2022-04-12 18:47:00 -0600

    Added Auth sub app

---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,24 @@
+import { mount } from "auth/AuthApp";
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+export default () => {
+  const ref = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathName }) => {
+        const pathName = history.location;
+        if (pathName !== nextPathName) {
+          history.push(nextPathName);
+          console.log("container navigated");
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
+
+  return <div ref={ref} />;
+};
